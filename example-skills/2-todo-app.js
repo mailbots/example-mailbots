ee1bf6da2fcfb8847ced692b0bded430c507a251
Reilly Sweetland <esweetland@gmail.com>
2018-09-07 15:44:24 -0700

    Add task.viewed handler

--- example-skills/2-todo-app.js
+++ example-skills/2-todo-app.js
@@ -187,7 +187,7 @@ module.exports = function(gopherApp) {
   });
 
   /**
-   * Handle schedul reminder action
+   * Handle complete reminder action
    */
   gopherApp.onAction("complete", function(gopher) {
     // Complete task in Todoist.
@@ -211,11 +211,11 @@ module.exports = function(gopherApp) {
    * Handle our reminder becomes due.
    * This can be simulated in the Sandbox.
    *
-   * NOTE: Much of this code is duplicated from
-   * the initial email. Read about Composing
-   * Gopher Skills for a more elegant solution.
+   * NOTE: This is nearly identical to the above onCommand
+   * handler. Find out about making reusable components here:
+   * https://github.com/gopherhq/gopher-app#making-reusable-skills
    */
-  gopherApp.on("task.reminder_due", function(gopher) {
+  gopherApp.onTrigger("todo", function(gopher) {
     gopher.webhook.addEmail({
       to: gopher.get("source.from"),
       subject: "Reminder: " + gopher.get("task.reference_email.subject"),
@@ -300,6 +300,109 @@ module.exports = function(gopherApp) {
         }
       ]
     });
+    gopher.webhook.respond();
+  });
+
+  /**
+   * Render a preview when someone views a todo item
+   * in the Gopher UI.
+   *
+   * NOTE: This is nearly identical to the above onCommand
+   * handler. Find out about making reusable components here:
+   * https://github.com/gopherhq/gopher-app#making-reusable-skills
+   */
+  gopherApp.on("task.viewed", function(gopher) {
+    gopher.webhook.addEmail({
+      to: gopher.get("source.from"),
+      from: "Gopher Todo",
+      subject: gopher.get("task.reference_email.subject"),
+      body: [
+        {
+          type: "html",
+          text: `<p>A user interacts with the below email preview and the actual
+          email in the same way. Try the action buttons below.</p><hr />`
+        },
+        {
+          type: "html",
+          text: `<h1>
+                    Todo: ${gopher.get("task.reference_email.subject")}
+                </h1>`
+        },
+        {
+          type: "button",
+          text: "Complete",
+          action: "complete",
+          style: "primary",
+          subject: "Hit Send to Complete this task",
+          body:
+            "This is a Gopher email-action, a way to get stuff done without leaving your inbox."
+        },
+        {
+          type: "button",
+          text: "View Task",
+          url: `https://app.gopher.email/task/${gopher.get("task.id")}`
+        },
+        {
+          type: "html",
+          text: `<table border=0" style="clear: both">
+                <tr>
+                    <td>
+                        <h4>NOTES</h4>
+                        <p>${gopher.webhook.getTaskData(
+                          "notes",
+                          "Add notes to this todo below"
+                        )}</p>
+                    </td>
+                </tr>
+            </table>`
+        },
+        {
+          type: "button",
+          text: "Add Notes",
+          action: "add_notes",
+          subject: "Add your notes to the email body"
+        },
+        {
+          type: "section"
+        },
+        {
+          type: "title",
+          text: "Schedule Reminder"
+        },
+        {
+          type: "button",
+          text: "tomorrow",
+          action: `remind.tomorrow`,
+          subject: `Schedule a reminder for tomorrow`,
+          body: ""
+        },
+        {
+          type: "button",
+          text: "3days",
+          action: `remind.3days`,
+          subject: `Schedule a reminder for 3 days`,
+          body: ""
+        },
+        {
+          type: "button",
+          text: "nextWeek",
+          action: `remind.nextWeek`,
+          subject: `Schedule a reminder for next week`,
+          body: ""
+        },
+        {
+          type: "button",
+          text: "nextMonth",
+          action: `remind.nextMonth`,
+          subject: `Schedule a reminder for next month`,
+          body: ""
+        },
+        {
+          type: "section"
+        }
+      ]
+    });
+
     gopher.webhook.respond();
   });
 };
